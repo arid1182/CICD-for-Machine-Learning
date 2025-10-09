ba878c7df3bf14741534eb7403b084961cafada3
Mubasher Hassan <arid1182@gmail.com>
2025-10-09 15:09:07 +0000

    Updated drug_app.py

--- App/drug_app.py
+++ App/drug_app.py
@@ -44,16 +44,16 @@ def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
 inputs = [
     gr.Slider(15, 74, step=1, label="Age"),
     gr.Radio(["M", "F"], label="Sex"),
-    gr.Radio(["HIGH", "LOW", "NORMAL"], label="Blood Pressure"),
-    gr.Radio(["HIGH", "NORMAL"], label="Cholesterol"),
+    gr.Radio(["high", "LOW", "NORMAL"], label="Blood Pressure"),
+    gr.Radio(["high", "NORMAL"], label="Cholesterol"),
     gr.Slider(6.2, 38.2, step=0.1, label="Na_to_K"),
 ]
 outputs = [gr.Label(num_top_classes=5)]
 
 examples = [
-    [30, "M", "HIGH", "NORMAL", 15.4],
+    [30, "M", "high", "NORMAL", 15.4],
     [35, "F", "LOW", "NORMAL", 8],
-    [50, "M", "HIGH", "HIGH", 34],
+    [50, "M", "high", "high", 34],
 ]
 
 
